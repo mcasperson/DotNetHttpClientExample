b5455e3206adb4f0072dcd850c0d90b1b8ec7f5a
Matthew Casperson <matthewcasperson@gmail.com>
2021-05-10 06:26:26 +1000

    Simple project created

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DotNetHttpClientExample
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            using var client = new HttpClient();
+            var result = await client.GetAsync(args[1]);
+            return (int) result.StatusCode >= 200 && (int) result.StatusCode <= 299 ? 0 : 1;
+        }
+    }
+}
